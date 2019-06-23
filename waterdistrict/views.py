133c70bd68a2ea274149adc4cbef056cc0af963f
Joe di Stefano <joeydi@okaypl.us>
2019-06-23 15:03:11 -0400

    add basic index page

--- waterdistrict/views.py
+++ waterdistrict/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import WaterReading
 
-# Create your views here.
+def index(request):
+    context = {
+        'title': "Richmond Fire District #1",
+        'latest_reading': WaterReading.objects.first(),
+        'readings': WaterReading.objects.all()[:10],
+    }
+    return render(request, 'index.html', context)
